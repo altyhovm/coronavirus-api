27e1c7df17daa2146a2696c158a0ba75f72fcd77
catofsof <58178903+catofsof@users.noreply.github.com>
2020-02-01 12:56:44 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,7 +1,8 @@
-const ncov = require('coronavirus-api')
+const ncov = require('./index')
 async function get() {
     console.log(await ncov.getNews());
     console.log(await ncov.getCountries());
+    console.log(await ncov.getСities());
     console.log(await ncov.getRussianSubjects());
 }
 
